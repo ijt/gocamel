9b8077143dd8d593bc861999c6f4ea6ec0465ea0
Issac Trotts <issac.trotts@gmail.com>
2022-01-28 19:46:44 -0700

    split out processOneFile for better testability

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
@@ -9,6 +10,7 @@ import (
 	"go/format"
 	"go/parser"
 	"go/token"
+	"io/ioutil"
 	"os"
 	"regexp"
 	"strings"
@@ -22,60 +24,60 @@ func main() {
 }
 
 func gocamel() error {
-	willPrintVars := flag.Bool("print_vars", false, "whether to print out the vars")
-	willPrint := flag.Bool("print", false, "whether to print out the AST")
+	willPrintAST := flag.Bool("print", false, "whether to print out the AST")
 	flag.Parse()
 	if flag.NArg() == 0 {
 		return errors.New("usage: gocamel file1.go ... fileN.go")
 	}
 
 	for _, filename := range flag.Args() {
-		f, err := os.Open(filename)
+		contents, err := ioutil.ReadFile(filename)
 		if err != nil {
-			return fmt.Errorf("opening %s: %w", filename, err)
-		}
-		// Create the AST by parsing src.
-		fset := token.NewFileSet() // positions are relative to fset
-		fileAST, err := parser.ParseFile(fset, filename, f, parser.ParseComments)
-		if err != nil {
-			return fmt.Errorf("parsing %s: %w", filename, err)
-		}
-		if err := f.Close(); err != nil {
-			return fmt.Errorf("closing %s: %w", filename, err)
+			return fmt.Errorf("reading %s: %w", filename, err)
 		}
 
-		if *willPrint {
-			if err := ast.Print(fset, fileAST); err != nil {
-				return fmt.Errorf("printing out AST: %w", err)
-			}
-		}
-
-		ast.Inspect(fileAST, func(n ast.Node) bool {
-			switch x := n.(type) {
-			case *ast.Ident:
-				if x.Obj != nil && x.Obj.Kind == ast.Var {
-					if *willPrintVars {
-						fmt.Printf("var: %v\n", x.Name)
-					}
-					x.Name = snakeToCamel(x.Name)
-				}
-			}
-			return true
-		})
-
-		// Overwrite the original file from the modified AST.
-		f, err = os.Create(filename)
+		newContents, err := processOneFile(filename, contents, *willPrintAST)
 		if err != nil {
-			return fmt.Errorf("opening %s for writing: %w", filename, err)
+			return fmt.Errorf("processing %s: %w", filename, err)
 		}
-		if err := format.Node(f, fset, fileAST); err != nil {
-			return fmt.Errorf("formatting AST for %s: %w", filename, err)
-		}
-		if err := f.Close(); err != nil {
-			return fmt.Errorf("closing %s: %w", filename, err)
+
+		if err := ioutil.WriteFile(filename, newContents, 0640); err != nil {
+			return fmt.Errorf("writing new version of %s: %w", filename, err)
 		}
 	}
 	return nil
+}
+
+func processOneFile(filename string, contents []byte, willPrintAST bool) ([]byte, error) {
+	// Create the AST by parsing src.
+	fset := token.NewFileSet() // positions are relative to fset
+	fileAST, err := parser.ParseFile(fset, filename, contents, parser.ParseComments)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s: %w", filename, err)
+	}
+
+	if willPrintAST {
+		if err := ast.Print(fset, fileAST); err != nil {
+			return nil, fmt.Errorf("printing out AST: %w", err)
+		}
+	}
+
+	ast.Inspect(fileAST, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.Ident:
+			if x.Obj != nil && x.Obj.Kind == ast.Var {
+				x.Name = snakeToCamel(x.Name)
+			}
+		}
+		return true
+	})
+
+	// Overwrite the original file from the modified AST.
+	var buf bytes.Buffer
+	if err := format.Node(&buf, fset, fileAST); err != nil {
+		return nil, fmt.Errorf("formatting AST for %s: %w", filename, err)
+	}
+	return buf.Bytes(), nil
 }
 
 var rx = regexp.MustCompile(`(\w)_(\w)`)
